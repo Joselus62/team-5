89fb3416543a269a2cbce01a0d5f0bd5f3ce3ef3
José Luis Elustondo <joselus62@yahoo.com.ar>
2022-11-22 18:22:25 -0300

    sin firebase

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,22 +1,24 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import Button from 'react-bootstrap/Button';
-
-
+import Home from './iconos/home-page.png'
+import Autor from './iconos/buscarAutores.jpg'
 export default function NavBar(props) {
     return (
         <>
         
             <nav className="nav">
+                        < img src={ Home } width={ 30 } height={ 30 } alt="logo"></img >
                         <Link className="navLink" to="/Login">Inicio</Link>
                         <Link className="navLink" to="/Libros">Lista de Libros</Link>
+                        < img src={Autor} width={ 30 } height={ 30 } alt="autor"></img>
                         <Link className="navLink" to="/Autor">Autores</Link>
                         <Link className="navLink" to="./Profile">Perfil</Link>
                         <Link className="navLink" to="/Categoría">Categorías</Link>
                         <div>
-                            <label className="navLabel" for="txtTitulo">Título del Libro: </label>
-                            <input type="text" className="input"></input>
-                          
+                            <label className="navLabel" htmlFor="txtTitulo">Título del Libro: </label>
+                            <input type="text" className="input" placeholder="Buscar"></input>
+                        
                         </div>
                         <Button type="submit" className="Enviar">Enviar</Button>{' '} 
                         <Link to="/"></Link>
